5ec0a712d77c405ee541e79f8922ca2f3779975a
Florencia <florencia.fierro@estudiante.uc.cl>
2025-09-07 16:26:17 -0300

    todo

---
+++ elements/jorge.py
@@ -0,0 +1,118 @@
+"""
+Hola este es modulo Jorge,
+este modulo manejara la creacion y movimiento de Jorge
+"""
+
+if __name__ == "__main__": # Solo para que no ejecutes este archivo
+    import sys
+    print(
+        "\033[38;2;255;0;0mESTE MODULO NO DEBE EJECUTARSE. EJECUTAR main.py\033[0m\n"
+        * 3
+    )
+    sys.exit()
+
+import pygame
+from pygame.locals import (
+    K_UP, K_DOWN, K_LEFT, K_RIGHT, RLEACCEL,K_w,K_a,K_s,K_d)
+import math
+
+from elements.projectile import Projectile
+
+
+JorgePNG = pygame.image.load('assets/JorgeVJ.png')
+JorgePNG_scaled = pygame.transform.scale(JorgePNG, (80, 80))
+
+
+class Player(pygame.sprite.Sprite):
+    
+    def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT):
+        super(Player, self).__init__()
+        self.surf = JorgePNG_scaled
+        self.surf.set_colorkey((0, 0, 0), RLEACCEL)
+        self.rect = self.surf.get_rect()
+        self.screen_width = SCREEN_WIDTH
+        self.screen_height = SCREEN_HEIGHT
+        self.vidas=3
+        self.base_speed=4
+        self.speed_time=0
+        self.speed_mult=2
+        self.vidas_recolectadas=0
+        self.chico=0
+        self.normal=self.surf.get_size()
+        self.imagen=pygame.transform.scale(JorgePNG, (80, 80))
+        self.surf=self.imagen.copy()
+        self.velocidades_recolectadas=0
+        self.invulnerable=False
+        self.invulnerable_hasta=0
+        self.disparo_triple_hasta=0
+        
+        # POR HACER (2.3): Crear lista de proyectiles
+        self.projectiles=pygame.sprite.Group()
+
+        #las balas tienen un cooldown de 1000 ticks
+        self.cooldown=1000
+        self.ultimo_tiro=0
+
+    def update(self, pressed_keys):
+        speed=self.base_speed
+        if pygame.time.get_ticks()<self.speed_time:
+            speed=int(self.base_speed*self.speed_mult)
+        if pressed_keys[K_w]:
+            self.rect.move_ip(0, -speed)
+        if pressed_keys[K_s]:
+            self.rect.move_ip(0, speed)
+        if pressed_keys[K_a]:
+            self.rect.move_ip(-speed, 0)
+        if pressed_keys[K_d]:
+            self.rect.move_ip(speed, 0)
+        
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > self.screen_width:
+            self.rect.right = self.screen_width
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > self.screen_height:
+            self.rect.bottom = self.screen_height
+        
+        # POR HACER (2.3): Actualizar la posición de los proyectiles
+        self.projectiles.update()
+
+        if self.invulnerable and pygame.time.get_ticks()>self.invulnerable_hasta:
+            self.invulnerable=False
+
+    
+    def shoot(self, mouse_pos): 
+        #si aun no se cumple el tiempo del cooldown, no se puede disparar
+        tiempo_actual=pygame.time.get_ticks()
+        if tiempo_actual-self.ultimo_tiro<self.cooldown:
+            return
+        
+        # POR HACER (2.3): Crear y calcular dirección proyectil
+        direction=(mouse_pos[0]-self.rect.centerx, mouse_pos[1]-self.rect.centery)
+        length=math.hypot(direction[0],direction[1])
+        if length==0:
+            return
+        direction=(direction[0]/length,direction[1]/length)
+
+        disparo_triple =tiempo_actual<=self.disparo_triple_hasta
+        if not disparo_triple:
+            projectile=Projectile(self.rect.center,direction,self.screen_width,self.screen_height)
+            self.projectiles.add(projectile)
+        else:
+            angulo=math.atan2(direction[0],direction[1])
+            d=math.radians(10)
+            for a in (angulo-d,angulo,angulo+d):
+                componente_x=math.cos(a)
+                componente_y=math.sin(a)
+                projectile=Projectile(self.rect.center,(componente_x,componente_y),self.screen_width,self.screen_height)
+                self.projectiles.add(projectile)
+
+        #actualizamos el tick del ultimo disparo
+        self.ultimo_tiro=tiempo_actual
+    
+    def perder_vida(self):
+        self.vidas-=1
+        
+
+        
---
+++ scenes/game.py
@@ -0,0 +1,394 @@
+'''
+Hola este es modulo game,
+este modulo manejara la escena donde ocurre nuestro juego
+'''
+
+if __name__ == "__main__": # Solo para que no ejecutes este archivo
+    import sys
+    print(
+        "\033[38;2;255;0;0mESTE MODULO NO DEBE EJECUTARSE. EJECUTAR main.py\033[0m\n"
+        * 3
+    )
+    sys.exit()
+
+import pygame
+
+from pygame.locals import (K_ESCAPE, KEYDOWN, QUIT)
+
+from elements.jorge import Player
+
+from elements.bug import Enemy
+
+from elements.mira import Mirilla
+
+from scenes.muerte import gameOver
+
+from elements.cronometro import Tiempo
+
+from scenes.inicio import pantalla_inicio
+
+from elements.enemigo2 import Enemy2
+
+from elements.enemigo3 import Enemy3
+
+from elements.velocidad import Speed
+powerups=pygame.sprite.Group()
+
+from elements.vidaextra import Life
+
+from elements.boss import Boss
+
+from scenes.victoria import pantalla_victoria
+
+
+
+def gameLoop():
+    ''' iniciamos los modulos de pygame'''
+    pygame.init()
+    pygame.font.init()
+    pygame.mixer.init()
+
+    fuente=pygame.font.SysFont("impact",30)
+    mensaje_vidas=""
+    vidas_tiempo=0
+    mensaje_velocidad=""
+    velocidad_tiempo=0
+    mensaje_disparo_triple=""
+    disparo_triple_tiempo=0
+
+    SCREEN_WIDTH = 1000
+    SCREEN_HEIGHT = 700
+    ''' 2.- crear el objeto pantalla'''
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    accion=pantalla_inicio()
+    if accion=="salir":
+        pygame.quit()
+        return
+    
+    if pygame.mixer.get_init():
+        pygame.mixer.music.load("assets/musica/musica.mp3")
+        pygame.mixer.music.play(-1)
+
+    sonido_powerup=pygame.mixer.Sound("assets/musica/powerup.mp3")
+    sonido_disparo=pygame.mixer.Sound("assets/musica/disparo.mp3")
+    sonido_victoria=pygame.mixer.Sound("assets/musica/victoria.mp3")
+
+    ''' Creamos y editamos la ventana de pygame (escena) '''
+    ''' 1.-definir el tamaño de la ventana'''
+    
+    background_image = pygame.image.load("assets/pixelBackground.jpg").convert()
+
+    ''' Preparamos el gameloop '''
+    #creamos las imagenes de los corazones, convert_alpha le quita el fondo negro a la imagen
+    corazon=pygame.image.load("assets/corazon.png").convert_alpha()
+    corazon=pygame.transform.scale(corazon,(50,50))
+
+    ''' 1.- creamos el reloj del juego'''
+
+    clock = pygame.time.Clock()
+    ''' 2.- generador de enemigos'''
+
+    ADDENEMY1 = pygame.USEREVENT + 1
+    pygame.time.set_timer(ADDENEMY1, 600)
+
+    ADDENEMY2 = pygame.USEREVENT + 2
+    pygame.time.set_timer(ADDENEMY2, 4500)
+
+    ADDENEMY3 = pygame.USEREVENT + 3
+    pygame.time.set_timer(ADDENEMY3, 3000)
+
+    ADDPOWER=pygame.USEREVENT+4
+    pygame.time.set_timer(ADDPOWER, 6000)
+
+    ADDLIFE=pygame.USEREVENT+5
+    pygame.time.set_timer(ADDLIFE,9000)
+
+    ''' 3.- creamos la instancia de jugador'''
+    player = Player(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    ''' 4.- contenedores de enemigos y jugador'''
+    enemies = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(player)
+
+    #creacion de la mira
+    mira=Mirilla(SCREEN_WIDTH,SCREEN_HEIGHT)
+    all_sprites.add(mira)
+
+    #creamos el cronometro
+    cronometro= Tiempo()
+
+    #creamos el puntaje
+    puntaje=0
+    letra_puntaje= pygame.font.SysFont("algerian", 25)
+
+    #variables para ver cuando aparece el boss
+    boss=0
+    aparicion_boss=False
+    muerte_boss=False
+    tiempo_victoria=0
+
+    ''' hora de hacer el gameloop '''
+    # variable booleana para manejar el loop
+    running = True
+
+    # loop principal del juego
+
+    while running:
+
+        screen.blit(background_image, [0, 0])
+        
+        for entity in all_sprites:
+            screen.blit(entity.surf, entity.rect)
+        
+        # POR HACER (2.5): Pintar proyectiles en pantalla
+        for projectile in player.projectiles:
+            screen.blit(projectile.surf,projectile.rect)
+        
+        #tenemos en pantalla la cantidad de vidas que tiene el jugador, las imagenes las separamos entre 50 
+        for i in range(player.vidas):
+            screen.blit(corazon, (10+i*50,10))
+        
+        # POR HACER (2.5): Eliminar bug si colisiona con proyectil
+        #tomamos el sistema de eliminaciones y lo vamos sumando al puntaje cada vez que matamos a un enemigo
+        eliminaciones=pygame.sprite.groupcollide(enemies,player.projectiles,True,True)
+        puntaje+=len(eliminaciones)
+
+        if puntaje>=50 and not hasattr(player,"triple_recibido"):
+            player.disparo_triple_hasta=pygame.time.get_ticks()+10000
+            mensaje_disparo_triple="Dispara triple por 10 segundos!"
+            disparo_triple_tiempo=pygame.time.get_ticks()+3000
+            player.triple_recibido=True
+
+        #si se llega a score 100 y aun no hay boss
+        if puntaje>=100 and aparicion_boss==False:
+            boss= Boss(SCREEN_WIDTH, SCREEN_HEIGHT, player)
+            all_sprites.add(boss)
+            aparicion_boss=True
+        
+        #si aparece el boss, spawnean menos enemigos
+        if aparicion_boss==True:
+            pygame.time.set_timer(ADDENEMY1,2000)
+            pygame.time.set_timer(ADDENEMY2,2000)
+            pygame.time.set_timer(ADDENEMY3,2000)
+        
+ 
+
+        
+        pressed_keys = pygame.key.get_pressed()
+        player.update(pressed_keys)
+        enemies.update()
+        powerups.update()
+        #actualizar la mira con el movimiento
+        mira.update()
+        if player.invulnerable and pygame.time.get_ticks()>player.invulnerable_hasta:
+            player.invulnerable=False
+        if player.chico != 0 and pygame.time.get_ticks()>player.chico:
+            a, b = player.normal
+            player.surf=pygame.transform.scale(player.imagen,(a,b))
+            centro=player.rect.center
+            player.rect=player.surf.get_rect(center=centro)
+            player.chico=0
+        
+        
+
+        
+        choque2= pygame.sprite.spritecollideany(player, powerups)
+        if choque2:
+            if isinstance(choque2,Speed):
+                player.speed_time=pygame.time.get_ticks()+5000
+                player.velocidades_recolectadas += 1
+                if player.velocidades_recolectadas==5:
+                    player.velocidades_recolectadas=0
+                    player.invulnerable=True
+                    player.invulnerable_hasta=pygame.time.get_ticks()+5000
+                    mensaje_velocidad="5 boosts de velocidad: Inmune por 5 segundos!"
+                    velocidad_tiempo=pygame.time.get_ticks()+3000
+            elif isinstance(choque2, Life):
+                player.vidas += 1
+                player.vidas_recolectadas += 1
+                if player.vidas_recolectadas==3:
+                    player.vidas_recolectadas=0
+                    player.chico=pygame.time.get_ticks()+5000
+                    a, b=player.normal
+                    chico=pygame.transform.scale(player.imagen,(a//2,b//2))
+                    player.surf=chico
+                    centro=player.rect.center
+                    player.rect=player.surf.get_rect(center=centro)
+                    mensaje_vidas="3 vidas extra: Pequeño por 5 segundos!"
+                    vidas_tiempo=pygame.time.get_ticks()+3000
+            sonido_powerup.play()
+            choque2.kill()
+        
+        if aparicion_boss==True and boss!=0:
+            boss.update()
+            screen.blit(boss.surf,boss.rect)
+            if player.rect.colliderect(boss.rect) and player.invulnerable==False:
+                player.perder_vida()
+                player.invulnerable=True
+                player.invulnerable_hasta=pygame.time.get_ticks()+30000
+                
+                if player.vidas<=0:
+                    #si tiene 0 vidad se termina el juego
+                    player.kill()
+                    pygame.mixer.music.stop()
+                    pygame.mixer.music.load("assets/perder/perder.mp3")
+                    pygame.mixer.music.play(-1)
+                    running = False
+                #si se muere, se empieza el loop de la escena de muerte
+                    accion2=gameOver()
+                    if accion2=="jugar":
+                        #si se quiere volver a jugar, debemos reiciar los datos
+                        player=Player(SCREEN_WIDTH,SCREEN_HEIGHT)
+                        enemies.empty()
+                        all_sprites.empty()
+                        all_sprites.add(player)
+                        all_sprites.add(mira)
+                        boss=0
+                        aparicion_boss=False
+                        muerte_boss=False
+                        tiempo_victoria=0
+                        puntaje=0
+                        cronometro=Tiempo()
+                        running=True
+                        pygame.mixer.music.stop()
+                        pygame.mixer.music.load("assets/musica/musica.mp3")
+                        pygame.mixer.music.play(-1)
+                    else:
+                        running=False
+
+            for disparo in player.projectiles:
+                if boss.rect.colliderect(disparo.rect):
+                    boss.perder_vida()
+                    disparo.kill()
+                    if boss.vidas==0:
+                        boss.kill()
+                        boss=0
+                        muerte_boss=True
+                        tiempo_victoria=pygame.time.get_ticks()
+            
+        if muerte_boss==True:
+            if pygame.time.get_ticks()-tiempo_victoria>=4000:
+                accion3= pantalla_victoria()
+                sonido_victoria.play()
+                if accion3=="jugar":
+                    player=Player(SCREEN_WIDTH,SCREEN_HEIGHT)
+                    enemies.empty()
+                    all_sprites.empty()
+                    all_sprites.add(player)
+                    all_sprites.add(mira)
+                    boss=0
+                    aparicion_boss=False
+                    muerte_boss=False
+                    tiempo_victoria=0
+                    puntaje=0
+                    cronometro=Tiempo()
+                    running=True
+                    pygame.mixer.music.stop()
+                    pygame.mixer.music.load("assets/musica/musica.mp3")
+                    pygame.mixer.music.play(-1)
+                else:
+                    running=False
+        
+        choque= pygame.sprite.spritecollideany(player, enemies)
+        if choque:
+            if not player.invulnerable:
+                #matamos al enemigo con el que choca y restamos una vida
+                choque.kill()
+                player.perder_vida()
+
+                if player.vidas<=0:
+                    #si tiene 0 vidad se termina el juego
+                    player.kill()
+                    pygame.mixer.music.stop()
+                    pygame.mixer.music.load("assets/perder/perder.mp3")
+                    pygame.mixer.music.play(-1)
+                    running = False
+                #si se muere, se empieza el loop de la escena de muerte
+                    accion2=gameOver()
+                    if accion2=="jugar":
+                        #si se quiere volver a jugar, debemos reiciar los datos
+                        player=Player(SCREEN_WIDTH,SCREEN_HEIGHT)
+                        enemies.empty()
+                        all_sprites.empty()
+                        all_sprites.add(player)
+                        all_sprites.add(mira)
+                        boss=0
+                        aparicion_boss=False
+                        muerte_boss=False
+                        tiempo_victoria=0
+                        puntaje=0
+                        cronometro=Tiempo()
+                        running=True
+                        pygame.mixer.music.stop()
+                        pygame.mixer.music.load("assets/musica/musica.mp3")
+                        pygame.mixer.music.play(-1)
+                    else:
+                        running=False
+            
+        cronometro.imagen(screen)
+
+        #dibujamos el puntaje abajo de los corazones
+        score=letra_puntaje.render(f"Score: {puntaje}", True, (255,255,255))
+        screen.blit(score, (10, 60))
+
+        if pygame.time.get_ticks()<vidas_tiempo and mensaje_vidas:
+            texto=fuente.render(mensaje_vidas,True,(255,0,255))
+            screen.blit(texto,(SCREEN_WIDTH//2-texto.get_width()//2,10))
+        
+        if pygame.time.get_ticks()<velocidad_tiempo and mensaje_velocidad:
+            texto2=fuente.render(mensaje_velocidad,True,(255,255,0))
+            screen.blit(texto2,(SCREEN_WIDTH//2-texto2.get_width()//2,SCREEN_HEIGHT-40))
+        
+        if pygame.time.get_ticks()<disparo_triple_tiempo and mensaje_disparo_triple:
+            texto3=fuente.render(mensaje_disparo_triple,True,(255,255,255))
+            screen.blit(texto3,(SCREEN_WIDTH//2-texto3.get_width()//2,SCREEN_HEIGHT//2-texto3.get_height()//2))
+
+        pygame.display.flip()
+        
+        # iteramos sobre cada evento en la cola
+        for event in pygame.event.get():
+            # se presiono una tecla?
+            if event.type == KEYDOWN:
+                # era la tecla de escape? -> entonces terminamos
+                if event.key == K_ESCAPE:
+                    running = False
+
+            # fue un click al cierre de la ventana? -> entonces terminamos
+            elif event.type == QUIT:
+                running = False
+            
+            elif event.type == ADDENEMY1:
+                new_enemy = Enemy(SCREEN_WIDTH, SCREEN_HEIGHT)
+                enemies.add(new_enemy)
+                all_sprites.add(new_enemy)
+            
+            elif event.type == ADDENEMY2:
+                new_enemy = Enemy2(SCREEN_WIDTH, SCREEN_HEIGHT)
+                enemies.add(new_enemy)
+                all_sprites.add(new_enemy)
+            
+            elif event.type == ADDENEMY3:
+                new_enemy = Enemy3(SCREEN_WIDTH, SCREEN_HEIGHT)
+                enemies.add(new_enemy)
+                all_sprites.add(new_enemy)
+            
+            elif event.type==ADDPOWER:
+                s=Speed(SCREEN_WIDTH,SCREEN_HEIGHT)
+                powerups.add(s)
+                all_sprites.add(s)
+            
+            elif event.type==ADDLIFE:
+                v=Life(SCREEN_WIDTH,SCREEN_HEIGHT)
+                powerups.add(v)
+                all_sprites.add(v)
+            
+            # POR HACER (2.4): Agregar evento disparo proyectil
+            elif event.type==pygame.MOUSEBUTTONDOWN:
+                player.shoot(pygame.mouse.get_pos())
+                sonido_disparo.play()
+
+
+        clock.tick(40)
